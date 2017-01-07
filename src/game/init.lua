4cda3448724662ca7ec049a605887cd19e7beb3a
aodota <442575816@qq.com>
2017-01-07 12:33:58 +0800

    移除无用资源

--- src/game/init.lua
+++ src/game/init.lua
@@ -6,9 +6,6 @@
 -- To change this template use File | Settings | File Templates.
 -- init 用来初始化该模块
 
--- 初始化Actions
-require "game.Net.Actions"
-
 -- 申明全局Tips
 cc.exports.Tips = require "game.Common.Tips"
 
